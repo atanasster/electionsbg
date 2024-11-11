63ca0c8d5deb3abfe852bd16be41f4f06e567280
atanasster <atanasster@gmail.com>
2024-11-11 17:55:00 -0500

    separate sections data to avoid unnecessary loading

--- src/data/ElectionsContext.tsx
+++ src/data/ElectionsContext.tsx
@@ -1,36 +1,16 @@
 import { useEffect, useState } from "react";
-import { PartyInfo, SectionInfo, Votes } from "./dataTypes";
-import { useAggregatedVotes } from "./AggregatedVotesHook";
+import { PartyInfo, Votes } from "./dataTypes";
 
 export const useElectionInfo = () => {
-  const [sections, setSections] = useState<SectionInfo[]>([]);
   const [parties, setParties] = useState<PartyInfo[]>([]);
-  const { votesBySettlement } = useAggregatedVotes();
   useEffect(() => {
-    fetch("/2024_10/sections.json")
-      .then((response) => response.json())
-      .then((data) => {
-        setSections(data);
-      });
     fetch("/2024_10/cik_parties.json")
       .then((response) => response.json())
       .then((data) => {
         setParties(data);
       });
   }, []);
-  const findSections = (
-    region: string,
-    municipality: string,
-    ekatte: string,
-  ) => {
-    const sectionCodes = votesBySettlement(region, municipality, ekatte);
-    if (!sectionCodes) {
-      return [];
-    }
-    return sections.filter((s) => {
-      return sectionCodes.sections.includes(s.section);
-    });
-  };
+
   const findParty = (partyNum: number) =>
     parties.find((p) => p.number === partyNum);
   const topVotesParty = (votes?: Votes[]) => {
@@ -44,5 +24,5 @@ export const useElectionInfo = () => {
     return tp ? findParty(tp.key) : null;
   };
 
-  return { findSections, findParty, topVotesParty };
+  return { findParty, topVotesParty };
 };
---
+++ src/data/SectionsContext.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { SectionInfo } from "./dataTypes";
+import { useAggregatedVotes } from "./AggregatedVotesHook";
+
+export const useSectionsInfo = () => {
+  const [sections, setSections] = useState<SectionInfo[]>([]);
+  const { votesBySettlement } = useAggregatedVotes();
+  useEffect(() => {
+    fetch("/2024_10/sections.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setSections(data);
+      });
+  }, []);
+  const findSections = (
+    region: string,
+    municipality: string,
+    ekatte: string,
+  ) => {
+    const sectionCodes = votesBySettlement(region, municipality, ekatte);
+    if (!sectionCodes) {
+      return [];
+    }
+    return sections.filter((s) => {
+      return sectionCodes.sections.includes(s.section);
+    });
+  };
+
+  return { findSections };
+};
