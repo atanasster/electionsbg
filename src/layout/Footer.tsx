48b1db0cdf3d8751b53e7ad83a812fd61a7b724f
atanasster <atanasster@gmail.com>
2024-11-20 18:20:25 -0500

    fix Link update

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -10,13 +10,13 @@ export const Footer = () => {
       </div>
       <ul className="flex flex-wrap items-center mt-3 text-md sm:mt-0">
         <li>
-          <Link href="#" aria-label="about" className="mx-2">
+          <Link to={{ pathname: "#" }} aria-label="about" className="mx-2">
             {t("about")}
           </Link>
         </li>
         <li>
           <Link
-            href="https://github.com/atanasster/data-bg"
+            to={{ pathname: "https://github.com/atanasster/data-bg" }}
             aria-label="github repository"
             className="mx-2"
           >
