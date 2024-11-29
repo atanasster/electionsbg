86c7b0d122efbe0ba85d83db9022a00dcab579d0
atanasster <atanasster@gmail.com>
2024-11-29 12:16:11 -0500

    fix optional fields

--- src/screens/reports/common/utils.ts
+++ src/screens/reports/common/utils.ts
@@ -58,26 +58,28 @@ export const calcReportRow = (
             obshtina,
             partyVotes,
             value,
-            voterTurnout:
-              100 *
-              (results.protocol.totalActualVoters /
-                results.protocol.numRegisteredVoters),
+            voterTurnout: results.protocol.numRegisteredVoters
+              ? 100 *
+                (results.protocol.totalActualVoters /
+                  results.protocol.numRegisteredVoters)
+              : 0,
             pctSupportsNoOne:
               100 *
-              ((results.protocol.numValidNoOnePaperVotes +
+              (((results.protocol.numValidNoOnePaperVotes || 0) +
                 (results.protocol.numValidNoOneMachineVotes || 0)) /
                 results.protocol.totalActualVoters),
             pctPartyVote:
               (100 * partyVotes.totalVotes) /
-              (results.protocol.numValidVotes +
+              ((results.protocol.numValidVotes || 0) +
                 (results.protocol.numValidMachineVotes || 0)),
-            pctInvalidBallots:
-              100 *
-              (results.protocol.numInvalidBallotsFound /
-                results.protocol.numPaperBallotsFound),
+            pctInvalidBallots: results.protocol.numPaperBallotsFound
+              ? 100 *
+                ((results.protocol.numInvalidBallotsFound || 0) /
+                  results.protocol.numPaperBallotsFound)
+              : 0,
             pctAdditionalVoters:
               100 *
-              (results.protocol.numAdditionalVoters /
+              ((results.protocol.numAdditionalVoters || 0) /
                 results.protocol.totalActualVoters),
             votes: results.votes,
             protocol: results.protocol,
