72ead305267188f648acf2f16b27dbe988bb3c60
atanasster <atanasster@gmail.com>
2024-11-30 03:51:26 -0500

    fixes for older elections

--- src/data/dataTypes.ts
+++ src/data/dataTypes.ts
@@ -100,3 +100,7 @@ export type PartyInfo = {
 };
 
 export type PartyVotes = PartyInfo & Votes;
+
+export const isMachineOnlyVote = (year: string) => {
+  return ["2021_07_11", "2021_11_14", "2022_10_02"].includes(year);
+};
