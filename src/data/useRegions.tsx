311168159df37227eeb760b414b42275be0f464f
atanasster <atanasster@gmail.com>
2024-12-03 06:44:29 -0500

    fix types

--- src/data/useRegions.tsx
+++ src/data/useRegions.tsx
@@ -1,9 +1,8 @@
 import regions from "../data/json/regions.json";
-import { RegionInfo } from "./useSettlements";
 import { useCallback } from "react";
 
 export const useRegions = () => {
-  const findRegion: (e?: string) => RegionInfo | undefined = useCallback(
+  const findRegion = useCallback(
     (e?: string) => (e ? regions?.find((s) => s.oblast == e) : undefined),
     [],
   );
