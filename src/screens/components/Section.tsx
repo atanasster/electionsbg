b0e74eaa6e3d988579067411469aefacbd02169e
atanasster <atanasster@gmail.com>
2024-12-05 03:55:28 -0500

    remove SectionVotes

--- src/screens/components/Section.tsx
+++ src/screens/components/Section.tsx
@@ -1,9 +1,9 @@
 import { FC } from "react";
 import { Caption } from "@/ux/Caption";
 import { SectionInfo } from "@/data/dataTypes";
-import { SectionVotes } from "./SectionVotes";
 import { ProtocolSummary } from "./ProtocolSummary";
 import { useTranslation } from "react-i18next";
+import { PartyVotes } from "./PartyVotes";
 
 export const Section: FC<{ section: SectionInfo }> = ({ section }) => {
   const { t } = useTranslation();
@@ -13,8 +13,8 @@ export const Section: FC<{ section: SectionInfo }> = ({ section }) => {
         <Caption>{`${t("section")} ${section.section}`}</Caption>
         <Caption className="mb-4">{`${section.settlement}${section.address ? `-${section.address}` : ""}`}</Caption>
         <ProtocolSummary protocol={section.protocol} votes={section.votes} />
-        {section.protocol && (
-          <SectionVotes protocol={section.protocol} votes={section.votes} />
+        {section.protocol && section.votes && (
+          <PartyVotes votes={section.votes} protocol={section.protocol} />
         )}
       </div>
     </div>
