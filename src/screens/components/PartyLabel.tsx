898b1d53e9774c5f0e50038c47061b0626264b02
atanasster <atanasster@gmail.com>
2024-12-05 05:59:52 -0500

    mobile resize table

--- src/screens/components/PartyLabel.tsx
+++ src/screens/components/PartyLabel.tsx
@@ -8,7 +8,7 @@ export const PartyLabel: FC<{ party?: PartyInfo }> = ({ party }) => {
   return (
     <Hint text={`${party ? party?.name : t("unknown_party")}`}>
       <div
-        className={`px-2 text-white font-bold`}
+        className={`px-2 text-white font-bold max-w-32`}
         style={{ backgroundColor: party?.color }}
       >
         {party?.nickName || t("unknown_party")}
