61f2640de9105654e582a3ca299a8fbc8bdfdf29
atanasster <atanasster@gmail.com>
2024-12-05 05:18:03 -0500

    underline style for hints

--- src/ux/Hint.tsx
+++ src/ux/Hint.tsx
@@ -1,7 +1,12 @@
 import { FC, PropsWithChildren } from "react";
 import { Tooltip } from "./Tooltip";
 
-export const Hint: FC<PropsWithChildren<{ text: string }>> = ({
-  text,
-  children,
-}) => <Tooltip content={<p>{text}</p>}>{children}</Tooltip>;
+export const Hint: FC<
+  PropsWithChildren<{ text: string; underline?: boolean }>
+> = ({ text, children, underline = true }) => (
+  <Tooltip content={<p>{text}</p>}>
+    <div className={`${underline ? "underline decoration-dashed" : ""}`}>
+      {children}
+    </div>
+  </Tooltip>
+);
