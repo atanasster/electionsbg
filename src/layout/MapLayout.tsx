a2dda3ec74d22efa99e47965d99ec9a0faa5a66c
atanasster <atanasster@gmail.com>
2024-12-05 04:42:44 -0500

    mobile map layout

--- src/layout/MapLayout.tsx
+++ src/layout/MapLayout.tsx
@@ -30,7 +30,10 @@ export const MapLayout: React.FC<{
   }, []);
   return (
     <div className={`w-full lg:pb-8 pb-2 px-4 md:px-8`}>
-      <div ref={refContainer} className="min-h-[100vh]">
+      <div
+        ref={refContainer}
+        className="min-h-96 md:min-h-[420px] lg:min-h-[600px]"
+      >
         {dimensions ? children(dimensions) : null}
       </div>
     </div>
