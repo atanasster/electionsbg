f614acf6e5fc22576faf856df5cb81387664dc20
atanasster <atanasster@gmail.com>
2024-12-05 23:48:54 -0500

    remove unused loading context

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import { Header } from "@/layout/Header";
 import { Footer } from "@/layout/Footer";
-import { Loader } from "@/ux/Loader";
-import { useLoading } from "@/ux/useLoading";
 
 export const Layout = (props: React.PropsWithChildren) => {
-  const { isLoading } = useLoading();
   return (
     <>
       <Header />
       <main className="min-h-[300] bg-card overflow-y-auto">
-        {isLoading && <Loader />}
         <div className="container flex flex-col justify-center items-center p-2 pt-12">
           {props.children}
         </div>
