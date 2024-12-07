80f35c33df8ccc5207aa859462a8ff3718f2ca3e
atanasster <atanasster@gmail.com>
2024-12-07 07:37:38 -0500

    election years structured

--- src/data/ElectionContext.tsx
+++ src/data/ElectionContext.tsx
@@ -1,17 +1,18 @@
 import { useCallback, useMemo } from "react";
-import elections from "../data/json/elections.json";
+import electionsData from "../data/json/elections.json";
 import { useSearchParams } from "react-router-dom";
 import { isMachineOnlyVote } from "./dataTypes";
 
 export const useElectionContext = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const elections = useMemo(() => electionsData.map((e) => e.name), []);
   const selected = useMemo(() => {
     const urlElections = searchParams.get("elections");
     if (urlElections && elections.find((e) => e === urlElections)) {
       return urlElections;
     }
     return elections[0];
-  }, [searchParams]);
+  }, [elections, searchParams]);
   const setSelected = useCallback(
     (newSelected: string) => {
       if (elections.find((e) => e === newSelected)) {
@@ -19,14 +20,14 @@ export const useElectionContext = () => {
         setSearchParams(searchParams, { replace: true });
       }
     },
-    [searchParams, setSearchParams],
+    [elections, searchParams, setSearchParams],
   );
   const priorElections: string | undefined = useMemo(() => {
     const idx = elections.findIndex((e) => e === selected);
     return idx >= 0 && idx < elections.length - 1
       ? elections[idx + 1]
       : undefined;
-  }, [selected]);
+  }, [elections, selected]);
 
   const isMachineOnly = () => isMachineOnlyVote(selected);
   return {
