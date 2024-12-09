1a2eb8846792b6b8c29ca8c65cb985b1c5607959
atanasster <atanasster@gmail.com>
2024-12-09 12:30:08 -0500

    section title

--- src/screens/SectionScreen.tsx
+++ src/screens/SectionScreen.tsx
@@ -1,15 +1,17 @@
 import { useSearchParams } from "react-router-dom";
 import { useSectionsInfo } from "@/data/useSectionsInfo";
 import { Section } from "./components/Section";
-import { Title } from "@/ux/Title";
 import { useSettlementsInfo } from "@/data/useSettlements";
 import { useTranslation } from "react-i18next";
 import { useMunicipalities } from "@/data/useMunicipalities";
 import { useRegions } from "@/data/useRegions";
+import { SEO } from "@/ux/SEO";
+import { H1 } from "@/ux/H1";
+import { Link } from "@/ux/Link";
 
 export const SectionScreen = () => {
   const [searchParams] = useSearchParams();
-  const { i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
   const { findSection } = useSectionsInfo();
   const { findSettlement } = useSettlementsInfo();
   const { findMunicipality } = useMunicipalities();
@@ -30,21 +32,59 @@ export const SectionScreen = () => {
 
   return (
     <div className={`w-full py-10 px-4 md:px-8`}>
-      <Title description="">
-        {`${
-          i18n.language === "bg"
-            ? region?.long_name || region?.name
-            : region?.long_name_en || region?.name_en || ""
-        }/${
-          i18n.language === "bg"
-            ? municipality?.long_name || municipality?.name
-            : municipality?.long_name_en || municipality?.name_en || ""
-        }/${
-          i18n.language === "bg"
-            ? settlement?.long_name || settlement?.name
-            : settlement?.long_name_en || settlement?.name_en || ""
-        }`}
-      </Title>
+      <SEO
+        title={`${t("section")} ${settlement ? (i18n.language === "bg" ? settlement?.name : settlement?.name_en) : ""}`}
+        description="Bulgaria election results in a set of polling stations"
+      />
+      <H1>
+        {t("section")}{" "}
+        {region?.oblast && (
+          <>
+            <Link
+              to={{
+                pathname: "/municipality",
+                search: {
+                  region: region?.oblast,
+                },
+              }}
+            >
+              {i18n.language === "bg"
+                ? region?.long_name || region?.name
+                : region?.long_name_en || region?.name_en}
+            </Link>
+            {" / "}
+          </>
+        )}
+        {municipality?.obshtina && (
+          <>
+            <Link
+              to={{
+                pathname: "/settlement",
+                search: {
+                  municipality: municipality?.obshtina,
+                },
+              }}
+            >
+              {i18n.language === "bg"
+                ? municipality?.name
+                : municipality?.name_en}
+            </Link>
+            {" / "}
+          </>
+        )}
+        {settlement && (
+          <Link
+            to={{
+              pathname: "/sections",
+              search: {
+                settlement: settlement?.ekatte,
+              },
+            }}
+          >
+            {i18n.language === "bg" ? settlement?.name : settlement?.name_en}
+          </Link>
+        )}
+      </H1>
       <Section section={section} />
     </div>
   );
