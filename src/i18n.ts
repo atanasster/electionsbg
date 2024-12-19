04f034bf4491c34d081bb583c1d29031a0b2e962
atanasster <atanasster@gmail.com>
2024-12-19 03:47:15 -0500

    default language bg

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,7 +3,6 @@ import { initReactI18next } from "react-i18next";
 
 import enTranslation from "src/locales/en/translation.json";
 import bgTranslation from "src/locales/bg/translation.json";
-
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
@@ -11,8 +10,8 @@ i18n
       en: { translation: enTranslation },
       bg: { translation: bgTranslation },
     },
-    lng: localStorage.getItem("language") === "bg" ? "bg" : "en",
-    fallbackLng: "en",
+    lng: localStorage.getItem("language") === "en" ? "en" : "bg",
+    fallbackLng: "bg",
 
     interpolation: {
       escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
