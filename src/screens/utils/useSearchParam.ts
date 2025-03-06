8047a57aefc385ab5f1676bcda0beb2ee734359b
atanasster <atanasster@gmail.com>
2025-03-06 16:36:37 -0500

    global persist view and recount

--- src/screens/utils/useSearchParam.ts
+++ src/screens/utils/useSearchParam.ts
@@ -4,11 +4,15 @@ import { NavigateOptions, useSearchParams } from "react-router-dom";
 export const useSearchParam = (
   param: string,
   navigateOpts?: NavigateOptions,
-): [string | null, (value: string) => void] => {
+): [string | null, (value?: string) => void] => {
   const [searchParams, setSearchParams] = useSearchParams();
   const setSearchParam = useCallback(
-    (newValue: string) => {
-      searchParams.set(param, newValue);
+    (newValue?: string) => {
+      if (newValue) {
+        searchParams.set(param, newValue);
+      } else {
+        searchParams.delete(param);
+      }
       setSearchParams(searchParams, navigateOpts);
     },
     [navigateOpts, param, searchParams, setSearchParams],
