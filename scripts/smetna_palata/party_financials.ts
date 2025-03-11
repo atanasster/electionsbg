cbb1beee1089d59bad1dd626f116af7608d8a6f5
atanasster <atanasster@gmail.com>
2025-03-11 15:12:19 +0200

    get candidate donations from global list

--- scripts/smetna_palata/party_financials.ts
+++ scripts/smetna_palata/party_financials.ts
@@ -3,14 +3,18 @@ import { parseDonors } from "./parse_donors";
 import { parseFromParties } from "./parse_parties";
 import { parseFromCandidates } from "./parse_candidates";
 import { parseFiling } from "./parse_filing";
+import { CandidateDonations } from "./parse_candidate_donations";
 
 export const parsePartyFinancing = async ({
   dataFolder,
   candidates,
+  party,
+  candidateDonations,
 }: {
   dataFolder: string;
   party: PartyInfo;
   candidates: CandidatesInfo[];
+  candidateDonations: CandidateDonations[];
 }) => {
   const filing = await parseFiling({ dataFolder });
   const fromDonors = await parseDonors({ income: filing.income, dataFolder });
@@ -20,8 +24,9 @@ export const parsePartyFinancing = async ({
   });
   const fromCandidates = await parseFromCandidates({
     income: filing.income,
-    dataFolder,
+    party,
     candidates,
+    candidateDonations,
   });
   return { fromDonors, fromParties, fromCandidates, filing };
 };
