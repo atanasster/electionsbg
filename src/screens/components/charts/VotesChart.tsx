566e2078cafb6ff547bcebbfed5f9a7ece73c973
atanasster <atanasster@gmail.com>
2025-04-05 11:30:22 +0300

    navigate to party page

--- src/screens/components/charts/VotesChart.tsx
+++ src/screens/components/charts/VotesChart.tsx
@@ -5,6 +5,7 @@ import { FC } from "react";
 import { useTranslation } from "react-i18next";
 import { formatPct, formatThousands } from "@/data/utils";
 import { useMediaQueryMatch } from "@/ux/useMediaQueryMatch";
+import { useNavigateParams } from "@/ux/useNavigateParams";
 
 const CustomTooltip: FC<{
   active?: boolean;
@@ -15,6 +16,7 @@ const CustomTooltip: FC<{
   label?: string;
 }> = ({ active, payload }) => {
   const { t } = useTranslation();
+
   return active && payload ? (
     <div className="z-50 overflow-hidden rounded-md bg-primary px-3 py-1.5 text-xs text-primary-foreground animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2">
       <div className="flex">
@@ -33,6 +35,7 @@ export const VotesChart: FC<{ votes?: PartyVotes[]; maxRows?: number }> = ({
   maxRows,
 }) => {
   const isLarge = useMediaQueryMatch("lg");
+  const navigate = useNavigateParams();
   const topValue = votes?.length ? votes[0].totalVotes : undefined;
   return (
     <ChartContainer config={{}} style={{ maxHeight: "200px" }}>
@@ -68,7 +71,12 @@ export const VotesChart: FC<{ votes?: PartyVotes[]; maxRows?: number }> = ({
         <ChartTooltip cursor={false} content={<CustomTooltip />} />
         <Bar dataKey="totalVotes" radius={8}>
           {votes?.map((p) => (
-            <Cell key={`cell-${p.partyNum}`} fill={p.color} />
+            <Cell
+              key={`cell-${p.partyNum}`}
+              fill={p.color}
+              cursor="pointer"
+              onClick={() => navigate({ pathname: `/party/${p.name}` })}
+            />
           ))}
           <LabelList
             filter={"url(#colored-bg)"}
